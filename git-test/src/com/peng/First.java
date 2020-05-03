a23ed2e1398088edd046bd625f07b88b54a97ea1
peng <822744993@qq.com>
2020-05-03 14:06:50 +0800

    本地工程测试

---
+++ git-test/src/com/peng/First.java
@@ -0,0 +1,11 @@
+package com.peng;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello git1 ");
+        System.out.println("hello git2 ");
+        System.out.println("hello git3 ");
+        System.out.println("hello git4 ");
+        System.out.println("hello git5 ");
+    }
+}
